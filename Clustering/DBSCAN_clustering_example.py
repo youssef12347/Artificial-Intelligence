2f01b6fc35559c4ea43fcaec88fd0193e7fe1bd1
youssef12347 <73423278+youssef12347@users.noreply.github.com>
2020-10-26 21:00:14 +0200

    Add files via upload

---
+++ Clustering/DBSCAN_clustering_example.py
@@ -0,0 +1,174 @@
+import pandas as pd
+import numpy as np
+
+pdf = pd.read_csv(r'C:\users\user\downloads\weather_station.csv')
+
+#cleaning (drop rows that have nulls in the Tm field.)
+pdf = pdf[pd.notnull(pdf["Tm"])]
+pdf = pdf.reset_index(drop=True)
+print(pdf.head(5))
+
+#plot
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from pylab import rcParams
+rcParams['figure.figsize'] = (14,10)
+
+llon=-140
+ulon=-50
+llat=40
+ulat=65
+
+pdf = pdf[(pdf['Long'] > llon) & (pdf['Long'] < ulon) & (pdf['Lat'] > llat) &(pdf['Lat'] < ulat)]
+
+my_map = Basemap(projection='merc',
+            resolution = 'l', area_thresh = 1000.0,
+            llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
+            urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+
+my_map.drawcoastlines()
+my_map.drawcountries()
+# my_map.drawmapboundary()
+my_map.fillcontinents(color = 'white', alpha = 0.3)
+my_map.shadedrelief()
+
+# To collect data based on stations        
+
+xs,ys = my_map(np.asarray(pdf.Long), np.asarray(pdf.Lat))
+pdf['xm']= xs.tolist()
+pdf['ym'] =ys.tolist()
+
+#Visualization1
+for index,row in pdf.iterrows():
+#   x,y = my_map(row.Long, row.Lat)
+   my_map.plot(row.xm, row.ym,markerfacecolor =([1,0,0]),  marker='o', markersize= 5, alpha = 0.75)
+#plt.text(x,y,stn)
+plt.show()
+
+
+
+
+#modeling DBscan
+from sklearn.cluster import DBSCAN
+import sklearn.utils
+from sklearn.preprocessing import StandardScaler
+sklearn.utils.check_random_state(1000)
+Clus_dataSet = pdf[['xm','ym']]
+Clus_dataSet = np.nan_to_num(Clus_dataSet)
+Clus_dataSet = StandardScaler().fit_transform(Clus_dataSet)
+
+# Compute DBSCAN
+db = DBSCAN(eps=0.15, min_samples=10).fit(Clus_dataSet)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+pdf["Clus_Db"]=labels
+
+realClusterNum=len(set(labels)) - (1 if -1 in labels else 0)
+clusterNum = len(set(labels)) 
+
+
+# A sample of clusters
+pdf[["Stn_Name","Tx","Tm","Clus_Db"]].head(5)
+
+
+#visualization of clusters based on location
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from pylab import rcParams
+rcParams['figure.figsize'] = (14,10)
+
+my_map = Basemap(projection='merc',
+            resolution = 'l', area_thresh = 1000.0,
+            llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
+            urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+
+my_map.drawcoastlines()
+my_map.drawcountries()
+#my_map.drawmapboundary()
+my_map.fillcontinents(color = 'white', alpha = 0.3)
+my_map.shadedrelief()
+
+# To create a color map
+colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, clusterNum))
+
+
+
+#Visualization1
+for clust_number in set(labels):
+    c=(([0.4,0.4,0.4]) if clust_number == -1 else colors[np.int(clust_number)])
+    clust_set = pdf[pdf.Clus_Db == clust_number]                    
+    my_map.scatter(clust_set.xm, clust_set.ym, color =c,  marker='o', s= 20, alpha = 0.85)
+    if clust_number != -1:
+        cenx=np.mean(clust_set.xm) 
+        ceny=np.mean(clust_set.ym) 
+        plt.text(cenx,ceny,str(clust_number), fontsize=25, color='red',)
+        print ("Cluster "+str(clust_number)+', Avg Temp: '+ str(np.mean(clust_set.Tm)))
+        
+        
+        
+        
+#re-run dbscan but 5D 
+#Clustering of stations based on their location, mean, max, and min Temperature
+from sklearn.cluster import DBSCAN
+import sklearn.utils
+from sklearn.preprocessing import StandardScaler
+sklearn.utils.check_random_state(1000)
+Clus_dataSet = pdf[['xm','ym','Tx','Tm','Tn']]
+Clus_dataSet = np.nan_to_num(Clus_dataSet)
+Clus_dataSet = StandardScaler().fit_transform(Clus_dataSet)
+
+# Compute DBSCAN
+db = DBSCAN(eps=0.3, min_samples=10).fit(Clus_dataSet)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+pdf["Clus_Db"]=labels
+
+realClusterNum=len(set(labels)) - (1 if -1 in labels else 0)
+clusterNum = len(set(labels)) 
+
+
+# A sample of clusters
+print(pdf[["Stn_Name","Tx","Tm","Clus_Db"]].head(5))
+
+
+
+#visualization based on location and temperature
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from pylab import rcParams
+rcParams['figure.figsize'] = (14,10)
+
+my_map = Basemap(projection='merc',
+            resolution = 'l', area_thresh = 1000.0,
+            llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
+            urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+
+my_map.drawcoastlines()
+my_map.drawcountries()
+#my_map.drawmapboundary()
+my_map.fillcontinents(color = 'white', alpha = 0.3)
+my_map.shadedrelief()
+
+# To create a color map
+colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, clusterNum))
+
+
+
+#Visualization1
+for clust_number in set(labels):
+    c=(([0.4,0.4,0.4]) if clust_number == -1 else colors[np.int(clust_number)])
+    clust_set = pdf[pdf.Clus_Db == clust_number]                    
+    my_map.scatter(clust_set.xm, clust_set.ym, color =c,  marker='o', s= 20, alpha = 0.85)
+    if clust_number != -1:
+        cenx=np.mean(clust_set.xm) 
+        ceny=np.mean(clust_set.ym) 
+        plt.text(cenx,ceny,str(clust_number), fontsize=25, color='red',)
+        print ("Cluster "+str(clust_number)+', Avg Temp: '+ str(np.mean(clust_set.Tm)))
+
+
+
+
+
+
